297ca5343114ab72befc2d308134264f1c1381a4
ericblair <eric.t.blair@gmail.com>
2011-05-20 14:29:21 +0100

    completed-chapter8

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,7 @@ describe UsersController do
 	render_views
 
   describe "GET 'new'" do
+  
     it "should be successful" do
       	get :new
       	response.should be_success
@@ -12,6 +13,26 @@ describe UsersController do
     it "should have the right title" do
     	get :new
     	response.should have_selector("title", :content => "Sign up")
+    end
+    
+    it "should have a name field" do
+    	get :new
+    	response.should have_selector("input[name='user[name]'][type='text']")
+    end
+    
+    it "should have an email field" do
+    	get :new
+    	response.should have_selector("input[name='user[email]'][type='text']")
+    end
+    
+    it "should have a password field" do
+    	get :new
+    	response.should have_selector("input[name='user[password]'][type='text']")
+    end
+    
+    it "should have a password confirmation field" do
+    	get :new
+    	response.should have_selector("input[name='user[password_confirmation]'][type='text']")
     end
   end
   
